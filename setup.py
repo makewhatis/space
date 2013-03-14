c8bdd4e66d5739158f0eaae5579b39fec51a1df1
David Johansen <david@makewhatis.com>
2013-03-14 10:26:33 -0700

    fixing requirements, and test suite.

--- setup.py
+++ setup.py
@@ -1,7 +1,48 @@
 #from distutils.core import setup
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup
+from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
+
+py_version = sys.version_info[:2]
+
+PY3 = py_version[0] == 3
+
+if PY3:
+    if py_version < (3, 2):
+        raise RuntimeError('On Python 3, Pyramid requires Python 3.2 or better')
+else:
+    if py_version < (2, 6):
+        raise RuntimeError('On Python 2, Pyramid requires Python 2.6 or better')
 
 version =  "0.0.1"
+tests_require = [
+    'mock >= 1.0.1',
+    'py',
+    'pytest',
+    'pytest-cov',
+    'coverage'
+    ]
+
+if not PY3:
+    tests_require.append('unittest2>=0.5.1')
+
+
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+         #import here, cause outside the eggs aren&#039;t loaded 
+        import pytest
+        errno = pytest.main('tests/unit')
+        sys.exit(errno)
+
+
 setup(name='space',
         version=version,
         description='Spacewalk Cli',
@@ -13,5 +54,7 @@ setup(name='space',
             'console_scripts': [
                 'space = space.main:main',
             ],
-        }
+        },
+        tests_require = tests_require,
+        cmdclass = {'test': PyTest},
       )
