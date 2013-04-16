eb448344ca94d85069a916a56cefcc049e4606a8
David Johansen <david@makewhatis.com>
2013-04-15 22:01:58 -0700

    fixing setup.py tests by adding multiprocessing import

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #from distutils.core import setup
+import multiprocessing
 import sys
 from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.test import test as TestCommand
+
 
 py_version = sys.version_info[:2]
 
@@ -26,7 +28,7 @@ tests_require = [
     ]
 
 if not PY3:
-    tests_require.append('unittest2>=0.5.1')
+    tests_require.append('unittest2')
 
 
 from setuptools.command.test import test as TestCommand
@@ -35,7 +37,12 @@ import sys
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['tests/unit', '--capture=sys']
+        self.test_args = [
+            'tests/unit',
+            '--capture=sys',
+            '--cov=space',
+            '--cov-report=term-missing'
+        ]
         self.test_suite = True
     def run_tests(self):
          #import here, cause outside the eggs aren&#039;t loaded 
